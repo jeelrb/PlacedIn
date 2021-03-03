29ad69a4fd20878f36ae09d5149de896f4a5d39d
placedIn7787 <placedIn7787@gmail.com>
2021-03-03 23:35:54 +0530

    Dashboard design continue...

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -6,10 +6,12 @@ function Post() {
         <MDBCol className="post pr-3 pb-4" md="7">
             <MDBContainer fluid className="post_container">
                 <MDBRow>
-                    <MDBCol className="post_buttons">
+                    <MDBCol md="6">
                         <MDBBtn className="post_button" href="#" color="default" size="lg">
                             Add Post
                         </MDBBtn>
+                    </MDBCol>
+                    <MDBCol md="6">
                         <MDBBtn className="post_button" href="#" color="default" size="lg">
                             Add Interview Experience
                         </MDBBtn>
